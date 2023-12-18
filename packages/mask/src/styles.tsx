886a5bdbe478c7efe8884ab43486e851a33506e4
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2023-12-18 16:39:06 +0530

    feat: add mask styles :sparkles:

---
+++ packages/mask/src/styles.tsx
@@ -0,0 +1,95 @@
+// Define a function type `StyleFn` for generating CSS properties.
+// It takes in two objects (props and state) and returns a React CSS properties object.
+export type StyleFn = (
+  props: { [key: string]: any },
+  state?: { [key: string]: any }
+) => React.CSSProperties
+
+// Define the different style functions available for different parts of the Mask component.
+export type Styles = {
+  maskWrapper: StyleFn
+  svgWrapper: StyleFn
+  maskArea: StyleFn
+  maskRect: StyleFn
+  clickArea: StyleFn
+  highlightedArea: StyleFn
+}
+
+// A type for a potentially partial styles object.
+export type StylesObj = Partial<Styles>
+
+// A type for valid keys of the Styles type.
+export type StyleKey = keyof Styles
+
+// This type is deprecated in favor of `StyleKey` for clearer naming.
+/**
+ * @deprecated Use `StyleKey` alias instead.
+ */
+export type StylesKeys = StyleKey
+
+// Define default styles for the Mask component. These styles can be overridden when using the component.
+export const defaultStyles: Styles = {
+  maskWrapper: () => ({
+    opacity: 0.7,
+    left: 0,
+    top: 0,
+    position: 'fixed',
+    zIndex: 99999,
+    pointerEvents: 'none',
+    color: '#000',
+  }),
+  svgWrapper: ({ windowWidth, windowHeight, wpx, wpy }) => ({
+    width: windowWidth,
+    height: windowHeight,
+    left: Number(wpx) / 2,
+    top: Number(wpy) / 2,
+    position: 'fixed',
+  }),
+  maskArea: ({ x, y, width, height }) => ({
+    x,
+    y,
+    width,
+    height,
+    fill: 'black',
+    rx: 0,
+  }),
+  maskRect: ({ windowWidth, windowHeight, maskID }) => ({
+    x: 0,
+    y: 0,
+    width: windowWidth,
+    height: windowHeight,
+    fill: 'currentColor',
+    mask: `url(#${maskID})`,
+  }),
+  clickArea: ({ windowWidth, windowHeight, clipID }) => ({
+    x: 0,
+    y: 0,
+    width: windowWidth,
+    height: windowHeight,
+    fill: 'currentcolor',
+    pointerEvents: 'auto',
+    clipPath: `url(#${clipID})`,
+  }),
+  highlightedArea: ({ x, y, width, height }) => ({
+    x,
+    y,
+    width,
+    height,
+    pointerEvents: 'auto',
+    fill: 'transparent',
+    display: 'none',
+  }),
+}
+
+// Type for a function that gets styles for a given key and extra data.
+export type getStylesType = (key: StylesKeys, extra?: any) => {}
+
+// A utility function to determine the appropriate styles based on the given key and state.
+// If a custom style function exists for the key, it's used. Otherwise, default styles are returned.
+export function stylesMatcher(styles: StylesObj) {
+  return (key: StyleKey, state: {}): any => {
+    const base = defaultStyles[key](state)
+    const custom = styles[key]
+    return custom ? custom(base, state) : base
+  }
+}
